a1142c0c310173e61d20f51aeb03996dd7ec8ecc
Lalo_Canche <eduardo_benji@hotmail.com>
2015-09-29 23:08:47 -0500

    Jquery validator agregado, pero no es usado (checa el archivo reservaciones.html, dejé un comentario)
    los eventos han sido cambiados

---
+++ Restaurante/public_html/js/Rules_for_validate.js
@@ -0,0 +1,38 @@
+/**
+ * Created by lalo on 29/09/15.
+ */
+
+
+$.validator.setDefaults({
+    submitHandler: function () {
+        alert("Datos Enviados!");
+    }
+});
+
+$(document).on('pageinit', function () { // <-- you must use this to ensure the DOM is ready
+
+    $("#reservaciones").find("form").validate({
+        rules: {
+            nombre: "required",
+            apellido: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            fecha: {
+                required: true
+            },
+            hora: "required"
+        },
+        messages: {
+            nombre: "Por favor, escriba su nombre",
+            apellido: "Por favor, escriba su apellido",
+            email:{
+                required: "Por favor, escriba su correo"
+            },
+            fecha: "Por favor, escriba una fecha válida",
+            hora:"Por favor, escriba una hora válida"
+        }
+    });
+
+});
